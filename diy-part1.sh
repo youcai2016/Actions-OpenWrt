a6b7451b5e9b7d1febfc0ac954dcf884b53177fc
youcai2016 <45659413+youcai2016@users.noreply.github.com>
2021-02-18 13:26:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 ##################################################################################################
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-git clone https://github.com/pymumu/luci-app-smartdns
